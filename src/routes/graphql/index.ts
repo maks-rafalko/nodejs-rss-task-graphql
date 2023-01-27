e95ca3458a6aab289d1053ce47e4d0f9b35c9782
Rafalko, Maksim <maks.rafalko@gmail.com>
2023-01-27 09:43:02 +0300

    refactor: add the very basic hello world GraphQL example

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,5 +1,20 @@
 import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-schema-to-ts';
 import { graphqlBodySchema } from './schema';
+import { graphql, buildSchema } from 'graphql';
+
+// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
+  type Query {
+    hello(name: String!): String
+  }
+`);
+
+// The rootValue provides a resolver function for each API endpoint
+const rootValue = {
+  hello: (args: any) => {
+    return `Hello world, ${args.name}!`;
+  },
+};
 
 const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
   fastify
@@ -12,9 +27,17 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
       },
     },
     async function (request, reply) {
-        // const { query, variables } = request.body;
-        //
-        // const result = await this.graphql(query, variables);
+      const { query, variables } = request.body;
+
+      // const result = await this.graphql(query, variables);
+
+      return await graphql({
+        schema,
+        source: String(query),
+        rootValue,
+        variableValues: variables,
+        contextValue: fastify
+      });
     }
   );
 };
