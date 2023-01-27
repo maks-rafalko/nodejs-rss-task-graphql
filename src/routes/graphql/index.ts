9d8cbf90d28d847ca1f4ff4fc824358299cf28ed
Rafalko, Maksim <maks.rafalko@gmail.com>
2023-01-27 10:38:50 +0300

    refactor: task 2.1 -  Get users, profiles, posts, memberTypes - 4 operations in one query.
    query:
    
    ```graphql
    query AllEntitiesIds {
        users {id},
        profiles {id},
        posts {id},
        memberTypes {id}
    }
    ```

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,18 +1,64 @@
 import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-schema-to-ts';
 import { graphqlBodySchema } from './schema';
 import { graphql, buildSchema } from 'graphql';
+import { FastifyInstance } from 'fastify';
 
 // Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
+  type User {
+    id: ID!
+    firstName: String
+    lastName: String
+    email: String
+    subscribedToUserIds: [Int!]!
+  }
+  
+  type Profile {
+    id: ID!
+    avatar: String!
+    sex: String!
+    birthday: Int!
+    country: String!
+    street: String!
+    city: String!
+    memberTypeId: String!
+    userId: String!
+  }
+  
+  type Post {
+    id: ID!
+    title: String!
+    content: String!
+    userId: String!
+  } 
+  
+  type MemberType {
+    id: ID!
+    discount: Int!,
+    monthPostsLimit: Int!,
+  }
+
   type Query {
-    hello(name: String!): String
+    users: [User!]!,
+    profiles: [Profile!]!,
+    posts: [Post!]!,
+    memberTypes: [MemberType!]!,
   }
 `);
 
 // The rootValue provides a resolver function for each API endpoint
 const rootValue = {
-  hello: (args: any) => {
-    return `Hello world, ${args.name}!`;
+  users: async (args: any, fastify: FastifyInstance) => {
+    return await fastify.db.users.findMany();
+  },
+  profiles: async (args: any, fastify: FastifyInstance) => {
+    return await fastify.db.profiles.findMany();
+  },
+  posts: async (args: any, fastify: FastifyInstance) => {
+    return await fastify.db.posts.findMany();
+  },
+  memberTypes: async (args: any, fastify: FastifyInstance) => {
+    return await fastify.db.memberTypes.findMany();
   },
 };
 
@@ -36,7 +82,7 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         source: String(query),
         rootValue,
         variableValues: variables,
-        contextValue: fastify
+        contextValue: fastify,
       });
     }
   );
