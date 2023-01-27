0e0e42c49f80f512ab16d4234155c4e48163f065
Rafalko, Maksim <maks.rafalko@gmail.com>
2023-01-27 14:37:23 +0300

    refactor: implement 2.3 Get users with their posts, profiles, memberTypes.
    query:
    
    ```graphql
    query UsersWithPostsProfilesMemberTypes {
        users {
            id,
            posts {
                id
            }
            profiles {
                id
            }
            memberTypes {
                id
            }
        }
    }
    ```

--- src/routes/graphql/user/userType.ts
+++ src/routes/graphql/user/userType.ts
@@ -1,4 +1,7 @@
 import {GraphQLID, GraphQLInt, GraphQLList, GraphQLObjectType, GraphQLString} from 'graphql';
+import {postType} from "../post/postType";
+import {profileType} from "../profile/profileType";
+import {memberTypeType} from "../memberType/memberTypeType";
 
 const userType = new GraphQLObjectType({
   name: 'User',
@@ -8,6 +11,30 @@ const userType = new GraphQLObjectType({
     lastName: { type: GraphQLString },
     email: { type: GraphQLString },
     subscribedToUserIds: { type: new GraphQLList(GraphQLInt) },
+    posts: {
+      type: new GraphQLList(postType),
+      resolve: async (user: any, args: any, fastify: any) => {
+        return await fastify.db.posts.findMany({key: 'userId', equals: user.id});
+      }
+    },
+    profiles: {
+      type: new GraphQLList(profileType),
+      resolve: async (user: any, args: any, fastify: any) => {
+        return await fastify.db.profiles.findMany({key: 'userId', equals: user.id});
+      }
+    },
+    memberTypes: {
+      type: new GraphQLList(memberTypeType),
+      resolve: async (user: any, args: any, fastify: any) => {
+        const profiles = await fastify.db.profiles.findMany({key: 'userId', equals: user.id});
+
+        if (profiles.length === 0) {
+          return Promise.resolve([]);
+        }
+
+        return await fastify.db.memberTypes.findMany({key: 'id', equals: profiles[0].memberTypeId});
+      }
+    }
   }
 });
 
