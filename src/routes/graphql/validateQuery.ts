62b32225ec413850e749232631ee53f7149e5339
Rafalko, Maksim <maks.rafalko@gmail.com>
2023-01-30 18:48:47 +0300

    refactor: add back specifiedRules - default validation rules from graphql

--- src/routes/graphql/validateQuery.ts
+++ src/routes/graphql/validateQuery.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from 'fastify';
 import { GraphQLError } from 'graphql/error/GraphQLError';
 import { DocumentNode, GraphQLSchema, parse, Source, validate } from 'graphql/index';
 import * as depthLimit from 'graphql-depth-limit';
+const { specifiedRules } = require('graphql/validation')
 
 const GRAPHQL_QUERY_DEPTH_LIMIT = 6;
 
@@ -16,6 +17,7 @@ const validateQuery = (schema: GraphQLSchema, queryAsString: string, fastify: Fa
   }
 
   const validationErrors = validate(schema, documentAST, [
+    ...specifiedRules,
     depthLimit(GRAPHQL_QUERY_DEPTH_LIMIT)
   ]);
 
